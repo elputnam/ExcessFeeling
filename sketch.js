e799557aeb8900b751ffb86f082fb107d2bc0b06
EL Putnam <elputnam@inaction.ie>
2022-12-30 16:51:06 +0000

    initial commit

---
+++ sketch.js
@@ -0,0 +1,91 @@
+//Animation for Trio Tan
+
+//Based on code from Dan Shiffman https://editor.p5js.org/codingtrain/sketches/qa7RiptE9
+
+let n = 0;
+let d = 0;
+let h = 0;
+let deg = 0;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 100);
+  angleMode(DEGREES);
+  frameRate(30);
+}
+
+function draw() {
+  background(180, 100, 10, 1);
+  push();
+  translate(width*.25, height*.25);
+  rose();
+  pop();
+
+  push();
+  translate(width*.5, height*.5);
+  rose();
+  pop();
+
+  push();
+  translate(width*.25, height*.75);
+  rose();
+  pop();
+
+  push();
+  translate(width*.75, height*.25);
+  rose();
+  pop();
+
+  push();
+  translate(width*.75, height*.75);
+  rose();
+  pop();
+
+
+
+  
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function rose(){
+  stroke(h, 50, 50);
+  strokeWeight(1);
+  noFill();
+  beginShape();
+  for (let i = 0; i < 361; i++){
+    let k = i * d;
+    let r = 300 * sin(n*k);
+    let x = r * cos(k);
+    let y = r * sin(k);
+    vertex(x, y)
+  }
+  endShape();
+
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+  beginShape();
+  for (let i = 0; i < 361; i++){
+    let k = i;
+    let r = 300 * sin(n*k);
+    let x = r * cos(k);
+    let y = r * sin(k);
+    vertex(x, y)
+  }
+  endShape();
+
+  n += 0.0001;
+  d += 0.0002;
+  h += 0.25;
+
+  if (h > 360){
+    h = 0;
+  }
+}
+
+function mousePressed(){
+  let fs = fullscreen();
+  fullscreen(!fs);
+}
